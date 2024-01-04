74c8a730357d05e80e8cd7f53c58e77f729847d0
KirillOrig <dpouldark@mail.ru>
2024-01-04 22:10:52 +0300

    Перебан ботов

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ввод количества элементов массива:
+
+      Console.Write("Введите количество элементов массива: ");
+      
+      int elementsCount = int.Parse(Console.ReadLine());
+      string[] bots = new string[elementsCount];
+      
+      // Ввод элементов массива:
+      
+      for (int i = 0; i < elementsCount; i++)
+      {
+        bots[i] = Console.ReadLine();
+      }
+
+      // Вывод строк баноффа
+
+      foreach (string s in bots) Console.WriteLine("/banoff 0 "+s+" 2000 Бот/Томаш");
+    }
+  }
+}
